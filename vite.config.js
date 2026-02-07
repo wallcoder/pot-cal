ed733c44b0ec68be22514067e2fef23ca60bf287
validdepix@gmail.com <biaka>
2026-02-07 16:48:45 +0530

    base change to pot-cal

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/penalty-ot-calculator',
+  base: '/pot-cal',
   plugins: [vue(), tailwindcss()],
 })
